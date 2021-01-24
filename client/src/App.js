acbd2cd0fd29676ba2d05f9e5df20253cb49fc7d
Ziyong He <ziyonghe94@gmail.com>
2021-01-24 17:07:43 -0500

    appjs route to component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { HashRouter, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
+import Saved from './components/Saved'
+import Result from './components/Result'
+import Search from './components/Search'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <HashRouter>
       <Navigation />
-    </div>
+      <div>
+        <Route exact path="/" component={Search}></Route>
+        <Route exact path="/result" component={Result}></Route>
+        <Route exact path="/saved" component={Saved}></Route>
+      </div>
+    </HashRouter>
   )
 }
 
---
+++ client/src/components/Saved.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Saved() {
+  return
+}
+
+export default Saved
