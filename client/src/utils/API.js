451427b156cda3e61d25b5cc8feff87934004a75
Ziyong He <ziyonghe94@gmail.com>
2021-01-25 00:07:25 -0500

    add client side API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,23 @@
 const key = API_KEY
 const API_KEY = 'AIzaSyDUw9dPFHVx5JKmA_EEKyYhlwRA68Y9rMs'
 
-export function searchBook() {}
+export function searchBook(title) {}
 
-export function saveBook() {}
+export function getSavedBooks() {
+  fetch('/saved').then((res) => res.json())
+}
 
-export function deleteBook() {}
+export function saveBook(data) {
+  fetch('/saved', {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ data }),
+  }).then((res) => res.json())
+  // change button here as saved indicator
+}
+
+export function deleteBook(title) {
+  fetch(`/:${title}`).then((res) => res.json())
+}
