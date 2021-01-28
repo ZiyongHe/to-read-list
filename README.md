4a0c060d7baeb626bca03b2140709e0d9cc6c20e
Ziyong He <ziyonghe94@gmail.com>
2021-01-28 12:59:24 -0500

    add readme

--- README.md
+++ README.md
@@ -1,29 +1,35 @@
-# Create React Express App
+# To-read List
 
-## About This Boilerplate
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
-This setup allows for a Node/Express/React app which can be easily deployed to Heroku.
+## Description
 
-The front-end React app will auto-reload as it's updated via webpack dev server, and the backend Express app will auto-reload independently with nodemon.
+This is a full stack web app that can search books through google book api, and return a list of books, further to view or save to save list. This app is built with MERN technologies (MongoDB, Express, React and Node.js).
 
-## Starting the app locally
+:link: [Link to deployed application](https://ziyonghe.github.io/employee-directory/)
 
-Start by installing front and backend dependencies. While in this directory, run the following command:
+![application preview](./client/src/utils/preview.PNG)
 
-```
-npm install
-```
+## Installation and Usage
 
-This should install node modules within the server and the client folder.
+1. For run the application locally run `npm install` command at working directory.
+2. Then run `npm start` to launch the web app.
 
-After both installations complete, run the following command in your terminal:
+**Search books and make a to-read list**
 
-```
-npm start
-```
+- Search by book title
+- Save to list
+- Delete when done
 
-Your app should now be running on <http://localhost:3000>. The Express server should intercept any AJAX requests from the client.
+## Built with
 
-## Deployment (Heroku)
+- Mongoose
+- Express
+- React
+- Node.js
+- React-Bootstrap
+- Google Books APIs
 
-To deploy, simply add and commit your changes, and push to Heroku. As is, the NPM scripts should take care of the rest.
+## License
+
+This application is covered under the **MIT license**.
--- client/src/components/Saved.js
+++ client/src/components/Saved.js
@@ -11,39 +11,42 @@ function Saved() {
 
   return (
     <Container>
-      {books
-        ? books.map((book, index) => {
-            return (
-              <Row className="p-3">
-                <Col xs={3}>
-                  <img src={book.image} alt={book.title} />
-                </Col>
-                <Col xs={8}>
-                  <h2>{book.title}</h2>
-                  <h5>{book.author}</h5>
-                  <p>{book.description}</p>
-                </Col>
-                <Col xs={1}>
-                  <input
-                    type="button"
-                    value="View"
-                    style={{ width: '70px' }}
-                    className="my-3"
-                    onClick={() => viewBook(book.link)}
-                  ></input>
-                  <input
-                    type="button"
-                    value="Delete"
-                    style={{ width: '70px' }}
-                    onClick={() => {
-                      deleteBook(books[index]._id)
-                    }}
-                  ></input>
-                </Col>
-              </Row>
-            )
-          })
-        : 'No books saved, go save some books!'}
+      <h3 className="row my-5 display-4 justify-content-md-center">
+        Saved Books
+      </h3>
+      <div className="row">
+        {books.map((book, index) => {
+          return (
+            <Row className="p-3">
+              <Col xs={3}>
+                <img src={book.image} alt={book.title} />
+              </Col>
+              <Col xs={8}>
+                <h2>{book.title}</h2>
+                <h5>{book.author}</h5>
+                <p>{book.description}</p>
+              </Col>
+              <Col xs={1}>
+                <input
+                  type="button"
+                  value="View"
+                  style={{ width: '70px' }}
+                  className="my-3"
+                  onClick={() => viewBook(book.link)}
+                ></input>
+                <input
+                  type="button"
+                  value="Delete"
+                  style={{ width: '70px' }}
+                  onClick={() => {
+                    deleteBook(books[index]._id)
+                  }}
+                ></input>
+              </Col>
+            </Row>
+          )
+        })}
+      </div>
     </Container>
   )
 }
